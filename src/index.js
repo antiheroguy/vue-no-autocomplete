cc4eccec96e703bcb2ac6a2e533862e183da132e
Mr. Hero <vndhero@gmail.com>
2021-09-27 07:38:34 +0700

    build: ver 1.0.3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-const directive = {
-  install(Vue) {
-    Vue.directive('no-autocomplete', {
-      bind(el, { value }) {
-        const isDisabled = el.disabled
-        el.disabled = true
-        setTimeout(() => {
-          el.disabled = isDisabled
-        }, 1000)
-      }
-    })
+export function install(Vue) {
+  if (install.installed) {
+    return
   }
+  install.installed = true
+
+  Vue.directive('no-autocomplete', {
+    bind(el, { value }) {
+      const isDisabled = el.disabled
+      el.disabled = true
+
+      element.style.backgroundColor = 'inherit'
+
+      setTimeout(() => {
+        el.disabled = isDisabled
+      }, 1000)
+    }
+  })
 }
 
-export default directive
+const plugin = {
+  install
+}
+
+let globalVue = null
+if (typeof window !== 'undefined') {
+  globalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  globalVue = global.Vue
+}
+if (globalVue) {
+  globalVue.use(plugin)
+}
+
+export default plugin
